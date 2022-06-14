d05433963a9c301f68dcb9746e05957935eb3407
Carlo1891 <carlo.18@live.it>
2022-06-14 15:18:14 +0200

    Inizio

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/* Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+/* L'output del prezzo finale andrà mostrato in forma umana in console (con massimo due decimali, per indicare centesimi sul prezzo)
